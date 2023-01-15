da71f91b40b5bc6722e31b8d67263b12756e978b
dongminghe <dongming1.he@gmail.com>
2023-01-15 15:32:14 +0800

    Gin搭建Blog API's 三

--- routers/router.go
+++ routers/router.go
@@ -2,7 +2,9 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"pss/middleware/jwt"
 	"pss/pkg/setting"
+	"pss/routers/api"
 	v1 "pss/routers/api/v1"
 )
 
@@ -15,7 +17,10 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
+	r.GET("/auth", api.GetAuth)
+
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)
@@ -25,6 +30,16 @@ func InitRouter() *gin.Engine {
 		apiv1.PUT("/tags/:id", v1.EditTag)
 		//删除指定标签
 		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+		//获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		//获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		//新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		//更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		//删除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 
 	return r
